dbc8ed5b976a633c820e396bbb22a3de3d102073
Ayush Chauhan <ayushchauhan.2076@gmail.com>
2026-07-05 22:48:32 +0530

    1. Implemented search method for BST 2. Implemented Inorder/Postorder/Preorder traversal for BST.

--- Assignment_25.py
+++ Assignment_25.py
@@ -22,8 +22,7 @@ class BST():
         return root
     
     def search(self, data):
-        temp = self.root
-        return self.rsearch(data, temp)
+        return self.rsearch(data, self.root)
     
     def rsearch(self, data, temp):
         if data==temp.item:
@@ -38,29 +37,47 @@ class BST():
     
     def inorder(self):
         output = []
-        self.rinorder(output,  self.root.left, self.root)
+        self.rinorder(output, self.root)
         return output
-    def rinorder(self, output = None, child = None, parent = None):
-        if child.left == None:
-            output.append(child.item)
-            if child.right == None:
-                output.append(parent.item)
-            elif child.right != None:
-                parent = parent.right
-                self.rinorder(output, parent.left, parent)
-        elif child.left != None:
-            self.rinorder(output, child.left, parent.left)
+    def rinorder(self, output, root):
+        if root != None:
+            self.rinorder(output, root.left)
+            output.append(root.item)
+            self.rinorder(output, root.right)
+    
+    def preorder(self):
+        output = []
+        self.rpreorder(self, output, self.root)
+        return output
+    def rpreorder(self, output, root):
+        if root != None:
+            output.append(root.item)
+            self.rinorder(output, root.left)
+            self.inorder(output, root.right)
+    
+    def postorder(self):
+        output = []
+        self.rpostorder(output, self.root)
+        return output
+    def rpostorder(self, output, root):
+        if root != None:
+            self.rpostorder(output, root.left)
+            self.rpostorder(output, root.right)
+            output.append(root.item)
 #driver_code
 t1 = BST()
 t1.insert(50)
 t1.insert(60)
+t1.insert(30)
 t1.insert(70)
 t1.insert(80)
-t1.insert(90)
-t1.insert(100)
 t1.insert(40)
-t1.insert(30)
-t1.insert(20)
 t1.insert(10)
+t1.insert(100)
+t1.insert(90)
+t1.insert(20)
+t1.insert(1)
 print(t1.search(50))
-print(t1.inorder())
+print("Inorder Output -- ", t1.inorder())
+print("Preorder Output -- ", t1.inorder())
+print("Postorder Output -- ", t1.postorder())
