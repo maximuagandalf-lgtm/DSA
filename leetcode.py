221c0b97c7341a6f528cc677482fee66d0efc7ea
Ayush Chauhan <ayushchauhan.2076@gmail.com>
2026-07-05 22:48:33 +0530

    1st leetcode problem

---
+++ leetcode.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+s1 = Solution()
+print(s1.twoSum([2,7,11,15], 9))
